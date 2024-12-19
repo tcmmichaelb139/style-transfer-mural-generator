38285a57889fd3127e07a7e298e5865ef0c6ce9e
Michael Bao <tcmb139@gmail.com>
2024-12-19 12:10:55 -0500

    initial version that transfers from style to style

--- main.py
+++ main.py
@@ -11,48 +11,13 @@ import torchvision.models as models
 
 from tqdm import tqdm
 
-NUM_STEPS = 500
+NUM_STEPS = 300
+LEARNING_RATE = 0.001
 ALPHA = 1
 BETA = 1e6
 
 device = "mps"
 torch.set_default_device(device)
-
-
-imageSize = 256
-
-loader = transforms.Compose([transforms.Resize(imageSize), transforms.ToTensor()])
-
-
-def imageLoader(image_name):
-    image = Image.open(image_name)
-    image = loader(image).unsqueeze(0)
-    return image.to(device, torch.float)
-
-
-styleImage = imageLoader("./tests/images/picasso.jpg")
-contentImage = imageLoader("./tests/images/dancing.jpg")
-
-assert styleImage.size() == contentImage.size(), "Image sizes need to be the same"
-
-unloader = transforms.ToPILImage()
-
-
-def imshow(tensor, title=None):
-    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
-    image = image.squeeze(0)  # remove the fake batch dimension
-    image = unloader(image)
-    plt.imshow(image)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)
-
-
-# plt.figure()
-# imshow(styleImage, title="Style Image")
-
-# plt.figure()
-# imshow(contentImage, title="Content Image")
 
 
 class ContentLoss(nn.Module):
@@ -65,20 +30,19 @@ class ContentLoss(nn.Module):
         return input
 
 
-def gramMatrix(input):
-    a, b, c, d = input.size()
-    features = input.view(a * b, c * d)
-    G = torch.mm(features, features.t())
-    return G.div(a * b * c * d)
-
-
 class StyleLoss(nn.Module):
-    def __init__(self, targetFeature):
+    def __init__(self, target):
         super(StyleLoss, self).__init__()
-        self.target = gramMatrix(targetFeature).detach()
+        self.target = self.gramMatrix(target.detach())
+
+    def gramMatrix(self, input):
+        a, b, c, d = input.size()
+        features = input.view(a * b, c * d)
+        G = torch.mm(features, features.t())
+        return G.div(a * b * c * d)
 
     def forward(self, input):
-        G = gramMatrix(input)
+        G = self.gramMatrix(input)
         self.loss = F.mse_loss(G, self.target)
         return input
 
@@ -97,49 +61,31 @@ class Normalization(nn.Module):
         return (img - self.mean) / self.std
 
 
-cnn = models.vgg19(weights="DEFAULT").features.to(device).eval()
+def getModelAndLosses(content, style):
+    vgg = models.vgg19(weights="DEFAULT").features.to(device).eval()
 
+    normalization = Normalization(normalizationMean, normalizationStd).to(device)
 
-content_layers_default = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
-style_layers_default = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
+    contentLayers = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
+    styleLayers = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
 
+    contentLosses = []
+    styleLosses = [[] for _ in range(len(style))]
 
-def get_style_model_and_losses(
-    cnn,
-    normalization_mean,
-    normalization_std,
-    style_img,
-    content_img,
-    content_layers=content_layers_default,
-    style_layers=style_layers_default,
-):
-    # normalization module
-    normalization = Normalization(normalization_mean, normalization_std)
-
-    # just in order to have an iterable access to or list of content/style
-    # losses
-    content_losses = []
-    style_losses = []
-
-    # assuming that ``cnn`` is a ``nn.Sequential``, so we make a new ``nn.Sequential``
-    # to put in modules that are supposed to be activated sequentially
     model = nn.Sequential(normalization)
 
-    i = 0  # increment every time we see a conv
-    for layer in cnn.children():
+    convNum = 0
+    for layer in vgg.children():
         if isinstance(layer, nn.Conv2d):
-            i += 1
-            name = "conv_{}".format(i)
+            convNum += 1
+            name = "conv_{}".format(convNum)
         elif isinstance(layer, nn.ReLU):
-            name = "relu_{}".format(i)
-            # The in-place version doesn't play very nicely with the ``ContentLoss``
-            # and ``StyleLoss`` we insert below. So we replace with out-of-place
-            # ones here.
+            name = "relu_{}".format(convNum)
             layer = nn.ReLU(inplace=False)
         elif isinstance(layer, nn.MaxPool2d):
-            name = "pool_{}".format(i)
+            name = "pool_{}".format(convNum)
         elif isinstance(layer, nn.BatchNorm2d):
-            name = "bn_{}".format(i)
+            name = "bn_{}".format(convNum)
         else:
             raise RuntimeError(
                 "Unrecognized layer: {}".format(layer.__class__.__name__)
@@ -147,136 +93,176 @@ def get_style_model_and_losses(
 
         model.add_module(name, layer)
 
-        if name in content_layers:
-            # add content loss:
-            target = model(content_img).detach()
-            content_loss = ContentLoss(target)
-            model.add_module("content_loss_{}".format(i), content_loss)
-            content_losses.append(content_loss)
+        if name in contentLayers:
+            target = model(content).detach()
+            contentLoss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(convNum), contentLoss)
+            contentLosses.append(contentLoss)
 
-        if name in style_layers:
-            # add style loss:
-            target_feature = model(style_img).detach()
-            style_loss = StyleLoss(target_feature)
-            model.add_module("style_loss_{}".format(i), style_loss)
-            style_losses.append(style_loss)
+        if name in styleLayers:
+            for i, styl in enumerate(style):
+                target = model(styl).detach()
+                styleLoss = StyleLoss(target)
+                model.add_module("style_loss_{}_{}".format(convNum, i), styleLoss)
+                styleLosses[i].append(styleLoss)
 
-    # now we trim off the layers after the last content and style losses
-    for i in range(len(model) - 1, -1, -1):
-        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+    for convNum in range(len(model) - 1, -1, -1):
+        if isinstance(model[convNum], ContentLoss) or isinstance(
+            model[convNum], StyleLoss
+        ):
             break
 
-    print(model)
-    model = model[: (i + 1)]
-    print(model, style_losses, content_losses)
+    model = model[: (convNum + 1)]
 
-    return model, style_losses, content_losses
+    return model, contentLosses, styleLosses
 
 
-inputImage = contentImage.clone()
-# if you want to use white noise by using the following code:
-#
-# .. code-block:: python
-#
-#    input_img = torch.randn(content_img.data.size())
+def runStyleTransfer(content, style, target, style1Weight=1):
+    target.requires_grad_(True)
 
-# add the original input image to the figure:
-# plt.figure()
-# imshow(inputImage, title="Input Image")
+    model, modelContentLosses, modelStyleLosses = getModelAndLosses(content, style)
 
-
-def get_input_optimizer(input_img):
-    # this line to show that input is a parameter that requires a gradient
-    optimizer = optim.Adam([input_img], lr=0.001)
-    return optimizer
-
-
-def run_style_transfer(
-    cnn,
-    normalization_mean,
-    normalization_std,
-    content_img,
-    style_img,
-    input_img,
-    num_steps=NUM_STEPS,
-):
-    """Run the style transfer."""
-    print("Building the style transfer model..")
-    model, style_losses, content_losses = get_style_model_and_losses(
-        cnn, normalization_mean, normalization_std, style_img, content_img
-    )
-
-    # We want to optimize the input and not the model parameters so we
-    # update all the requires_grad fields accordingly
-    input_img.requires_grad_(True)
-    # We also put the model in evaluation mode, so that specific layers
-    # such as dropout or batch normalization layers behave correctly.
     model.eval()
     model.requires_grad_(False)
 
-    optimizer = get_input_optimizer(input_img)
+    optimizer = optim.Adam([target], LEARNING_RATE)
 
-    print("Optimizing..")
+    for step in tqdm(range(NUM_STEPS)):
+        with torch.no_grad():
+            target.clamp_(0, 1)
 
-    for step in tqdm(range(num_steps)):
+        optimizer.zero_grad()
 
-        def closure():
-            # correct the values of updated input image
-            with torch.no_grad():
-                input_img.clamp_(0, 1)
+        model(target)
 
-            optimizer.zero_grad()
-            model(input_img)
-            style_score = 0
-            content_score = 0
+        contentLoss = 0
+        styleLoss = [0 for _ in range(len(style))]
 
-            for sl in style_losses:
-                style_score += sl.loss
-            for cl in content_losses:
-                content_score += cl.loss
+        for loss in modelContentLosses:
+            contentLoss += loss.loss
 
-            loss = ALPHA * content_score + BETA * style_score
-            loss.backward()
+        for i in range(len(style)):
+            for loss in modelStyleLosses[i]:
+                styleLoss[i] += loss.loss
 
-            if step % 50 == 0:
-                print("run {}:".format(step))
-                print(
-                    "Style Loss : {:4f} Content Loss: {:4f}".format(
-                        style_score.item(), content_score.item()
-                    )
+        totalLoss = ALPHA * contentLoss + BETA * (
+            style1Weight * styleLoss[0] + ((1 - style1Weight) * styleLoss[1])
+        )
+        totalLoss.backward()
+
+        optimizer.step()
+
+        if step % 100 == 0:
+            print("run {}:".format(step))
+            print(
+                "Style Losses : {:4f} | {:4f} Content Loss: {:4f}".format(
+                    styleLoss[0].item(), styleLoss[1].item(), contentLoss.item()
                 )
-                print()
+            )
+            print()
 
-            return style_score + content_score
+    target.requires_grad_(False)
 
-        optimizer.step(closure)
-
-    # a last correction...
-    with torch.no_grad():
-        input_img.clamp_(0, 1)
-
-    return input_img
+    return target
 
 
-output = run_style_transfer(
-    cnn,
-    normalizationMean,
-    normalizationStd,
-    contentImage,
-    styleImage,
-    inputImage,
+def importImages(contentImageLoc, styleImageLoc):
+    contentLoader = transforms.ToTensor()
+
+    contentImage = Image.open(contentImageLoc)
+    contentImage = contentLoader(contentImage).unsqueeze(0).to(device)
+
+    imageSizeH = contentImage.size()[2]
+    imageSizeW = contentImage.size()[3]
+
+    styleLoader = transforms.Compose(
+        [transforms.Resize((imageSizeH, imageSizeW)), transforms.ToTensor()]
+    )
+
+    styleImages = []
+    for loc in styleImageLoc:
+        image = Image.open(loc)
+        image = styleLoader(image).unsqueeze(0).to(device)
+        styleImages.append(image)
+
+    return contentImage, styleImages
+
+
+def splitImages(contentImage, styleImages, splits=10):
+    _, __, imageSizeH, imageSizeW = contentImage.size()
+
+    splitWidth = imageSizeW // splits + 1
+
+    contentImage = contentImage.split(splitWidth, dim=3)
+    styleImages = [style.split(splitWidth, dim=3) for style in styleImages]
+
+    return contentImage, styleImages
+
+
+def getSplitStyleTransfer(contentImage, styleImages, style1Weight=1):
+    targetImage = contentImage.clone()
+    targetImage = runStyleTransfer(contentImage, styleImages, targetImage, style1Weight)
+
+    return targetImage
+
+
+def runSplitStyleTransfer(contentImage, styleImages, style1Weight=1, splits=10):
+    splitContentImages, splitStyleImages = splitImages(
+        contentImage, styleImages, splits
+    )
+
+    targetImages = []
+
+    for i, (content, style1, style2) in enumerate(
+        zip(splitContentImages, splitStyleImages[0], splitStyleImages[1])
+    ):
+        targetImage = content.clone()
+        targetImage = runStyleTransfer(
+            content, [style1, style2], targetImage, i * (1 / (splits - 1))
+        )
+        targetImages.append(targetImage)
+
+    return targetImages
+
+
+contentLoc = "./tests/images/dancing.jpg"
+styleLoc = ["./tests/images/picasso.jpg", "./tests/images/picasso2.jpg"]
+
+contentImage, styleImages = importImages(contentLoc, styleLoc)
+
+targetImages = runSplitStyleTransfer(
+    contentImage, styleImages, style1Weight=1, splits=10
 )
 
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-ax[0].imshow(unloader(styleImage.squeeze(0)))
-ax[0].set_title("Style Image")
-ax[0].axis("off")
-ax[1].imshow(unloader(contentImage.squeeze(0)))
-ax[1].set_title("Content Image")
-ax[1].axis("off")
-ax[2].imshow(unloader(output.squeeze(0)))
-ax[2].set_title("Output Image")
-ax[2].axis("off")
+targetImages = torch.cat(targetImages, dim=3)
 
-plt.ioff()
+unloader = transforms.ToPILImage()
+
+plt.imshow(unloader(targetImages.squeeze(0)))
+plt.axis("off")
 plt.show()
+
+# assert styleImage[0].size() == contentImage.size(), "Image sizes need to be the same"
+# assert styleImage[1].size() == contentImage.size(), "Image sizes need to be the same"
+
+# unloader = transforms.ToPILImage()
+
+# targetImage = runStyleTransfer(contentImage, styleImage, targetImage, 1)
+
+
+# fig, ax = plt.subplots(1, 4, figsize=(15, 5))
+# ax[0].imshow(unloader(styleImage[0].squeeze(0)))
+# ax[0].set_title("Style Image")
+# ax[0].axis("off")
+# ax[1].imshow(unloader(styleImage[1].squeeze(0)))
+# ax[1].set_title("Style Image 2")
+# ax[1].axis("off")
+# ax[2].imshow(unloader(contentImage.squeeze(0)))
+# ax[2].set_title("Content Image")
+# ax[2].axis("off")
+# ax[3].imshow(unloader(targetImage.squeeze(0)))
+# ax[3].set_title("Output Image")
+# ax[3].axis("off")
+
+# plt.ioff()
+# plt.show()
