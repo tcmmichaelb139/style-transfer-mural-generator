3e9d0204b2473ebaad0748bd740f997e3b0c7c64
Michael Bao <tcmb139@gmail.com>
2024-12-20 15:02:38 -0500

    working version (before adding interface)

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import torchvision.models as models
 
 from tqdm import tqdm
 
-NUM_STEPS = 300
-LEARNING_RATE = 0.001
+NUM_STEPS = 1000
+LEARNING_RATE = 1
 ALPHA = 1
-BETA = 1e6
+BETA = 1e5
 
 device = "mps"
 torch.set_default_device(device)
@@ -30,19 +30,21 @@ class ContentLoss(nn.Module):
         return input
 
 
+def gramMatrix(input):
+    (b, ch, h, w) = input.size()
+    features = input.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+
 class StyleLoss(nn.Module):
     def __init__(self, target):
         super(StyleLoss, self).__init__()
-        self.target = self.gramMatrix(target.detach())
-
-    def gramMatrix(self, input):
-        a, b, c, d = input.size()
-        features = input.view(a * b, c * d)
-        G = torch.mm(features, features.t())
-        return G.div(a * b * c * d)
+        self.target = gramMatrix(target).detach()
 
     def forward(self, input):
-        G = self.gramMatrix(input)
+        G = gramMatrix(input)
         self.loss = F.mse_loss(G, self.target)
         return input
 
@@ -62,12 +64,12 @@ class Normalization(nn.Module):
 
 
 def getModelAndLosses(content, style):
-    vgg = models.vgg19(weights="DEFAULT").features.to(device).eval()
+    vgg = models.vgg16(weights="DEFAULT").features.to(device).eval()
 
     normalization = Normalization(normalizationMean, normalizationStd).to(device)
 
-    contentLayers = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
-    styleLayers = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
+    contentLayers = ["relu_1", "relu_2", "relu_3", "relu_4"]
+    styleLayers = ["relu_1", "relu_2", "relu_3", "relu_4"]
 
     contentLosses = []
     styleLosses = [[] for _ in range(len(style))]
@@ -127,7 +129,7 @@ def runStyleTransfer(content, style, target, style1Weight=1):
 
     optimizer = optim.Adam([target], LEARNING_RATE)
 
-    for step in tqdm(range(NUM_STEPS)):
+    for step in tqdm(range(1, NUM_STEPS + 1)):
         with torch.no_grad():
             target.clamp_(0, 1)
 
@@ -148,67 +150,103 @@ def runStyleTransfer(content, style, target, style1Weight=1):
         totalLoss = ALPHA * contentLoss + BETA * (
             style1Weight * styleLoss[0] + ((1 - style1Weight) * styleLoss[1])
         )
+
         totalLoss.backward()
 
         optimizer.step()
 
-        if step % 100 == 0:
-            print("run {}:".format(step))
-            print(
-                "Style Losses : {:4f} | {:4f} Content Loss: {:4f}".format(
-                    styleLoss[0].item(), styleLoss[1].item(), contentLoss.item()
-                )
-            )
-            print()
+    with torch.no_grad():
+        target.clamp_(0, 1)
 
     target.requires_grad_(False)
 
     return target
 
 
-def importImages(contentImageLoc, styleImageLoc):
-    contentLoader = transforms.ToTensor()
+def importImages(contentImageLoc, styleImageLoc, splits, overlap, scale):
+    contentImage = Image.open(contentImageLoc).convert("RGB")
+    imageSizeW, imageSizeH = contentImage.size
 
-    contentImage = Image.open(contentImageLoc)
-    contentImage = contentLoader(contentImage).unsqueeze(0).to(device)
+    imageSizeW = int(imageSizeW * scale)
+    imageSizeH = int(imageSizeH * scale)
 
-    imageSizeH = contentImage.size()[2]
-    imageSizeW = contentImage.size()[3]
+    rescale = transforms.Resize((imageSizeH, imageSizeW))
 
-    styleLoader = transforms.Compose(
+    contentImage = rescale(contentImage)
+
+    if splits != 0:
+        splitWidth = (imageSizeW + (splits - 1) * overlap) // splits
+
+        imageSizeH = (
+            imageSizeH * (splitWidth * splits - overlap * (splits - 1))
+        ) // imageSizeW
+        imageSizeW = splitWidth * splits - overlap * (splits - 1)
+
+    loader = transforms.Compose(
         [transforms.Resize((imageSizeH, imageSizeW)), transforms.ToTensor()]
     )
 
+    contentImage = loader(contentImage).unsqueeze(0).to(device)
+
     styleImages = []
     for loc in styleImageLoc:
-        image = Image.open(loc)
-        image = styleLoader(image).unsqueeze(0).to(device)
+        image = Image.open(loc).convert("RGB")
+        image = loader(image).unsqueeze(0).to(device)
         styleImages.append(image)
 
     return contentImage, styleImages
 
 
-def splitImages(contentImage, styleImages, splits=10):
+def getSplitImages(contentImage, styleImages, splits, overlap):
     _, __, imageSizeH, imageSizeW = contentImage.size()
 
-    splitWidth = imageSizeW // splits + 1
+    if splits == 0:
+        contentImage = contentImage.unsqueeze(0)
+        styleImages = [style.unsqueeze(0) for style in styleImages]
+        return contentImage, styleImages
 
-    contentImage = contentImage.split(splitWidth, dim=3)
-    styleImages = [style.split(splitWidth, dim=3) for style in styleImages]
+    splitWidth = (imageSizeW + (splits - 1) * overlap) // splits
+
+    contentImage = contentImage.unfold(3, splitWidth, splitWidth - overlap)
+    contentImage = contentImage.movedim(3, 0)
+
+    assert contentImage.size()[0] == splits, "Splitting failed"
+
+    styleImages = [
+        style.unfold(3, splitWidth, splitWidth - overlap).movedim(3, 0)
+        for style in styleImages
+    ]
 
     return contentImage, styleImages
 
 
-def getSplitStyleTransfer(contentImage, styleImages, style1Weight=1):
-    targetImage = contentImage.clone()
-    targetImage = runStyleTransfer(contentImage, styleImages, targetImage, style1Weight)
+def joinSplitImages(splitImages, overlap):
+    targetImages = splitImages[0]
 
-    return targetImage
+    for i in range(1, len(splitImages)):
+        weights = torch.linspace(0, 1, overlap).to(device)
+        weights = weights.view(1, 1, 1, overlap)
+
+        targetImages[:, :, :, -overlap:] = torch.lerp(
+            targetImages[:, :, :, -overlap:],
+            splitImages[i][:, :, :, 0:overlap],
+            weights,
+        )
+
+        targetImages = torch.cat(
+            (targetImages, splitImages[i][:, :, :, overlap:]), dim=3
+        )
+
+    return targetImages
 
 
-def runSplitStyleTransfer(contentImage, styleImages, style1Weight=1, splits=10):
-    splitContentImages, splitStyleImages = splitImages(
-        contentImage, styleImages, splits
+def runSplitStyleTransfer(contentLoc, styleLoc, splits=10, overlap=1, scale=1):
+    contentImage, styleImages = importImages(
+        contentLoc, styleLoc, splits, overlap, scale
+    )
+
+    splitContentImages, splitStyleImages = getSplitImages(
+        contentImage, styleImages, splits, overlap
     )
 
     targetImages = []
@@ -216,53 +254,51 @@ def runSplitStyleTransfer(contentImage, styleImages, style1Weight=1, splits=10):
     for i, (content, style1, style2) in enumerate(
         zip(splitContentImages, splitStyleImages[0], splitStyleImages[1])
     ):
+        print("Running split {}".format(i))
         targetImage = content.clone()
         targetImage = runStyleTransfer(
-            content, [style1, style2], targetImage, i * (1 / (splits - 1))
+            content, [style1, style2], targetImage, 1 - i * (1 / (splits - 1))
         )
         targetImages.append(targetImage)
 
-    return targetImages
+    targetImage = joinSplitImages(targetImages, overlap)
+
+    return contentImage, styleImages, targetImage
 
 
-contentLoc = "./tests/images/dancing.jpg"
-styleLoc = ["./tests/images/picasso.jpg", "./tests/images/picasso2.jpg"]
+def getDimensions(loc):
+    image = Image.open(loc).convert("RGB")
+    imageSizeW, imageSizeH = image.size
+    return imageSizeW, imageSizeH
 
-contentImage, styleImages = importImages(contentLoc, styleLoc)
 
-targetImages = runSplitStyleTransfer(
-    contentImage, styleImages, style1Weight=1, splits=10
+# contentLoc = "./tests/images/dancing.jpg"
+contentLoc = "./tests/images/PixelartCity.jpeg"
+# styleLoc = ["./tests/images/picasso.jpg", "./tests/images/picasso2.jpg"]
+# contentLoc = "./tests/images/amber.jpg"
+styleLoc = ["./tests/images/rain-princess-cropped.jpg", "./tests/images/picasso.jpg"]
+# styleLoc = ["./tests/images/picasso.jpg", "./tests/images/PixelartCity.jpeg"]
+
+
+contentImage, styleImages, targetImage = runSplitStyleTransfer(
+    contentLoc, styleLoc, splits=20, overlap=10, scale=1
 )
-
-targetImages = torch.cat(targetImages, dim=3)
 
 unloader = transforms.ToPILImage()
 
-plt.imshow(unloader(targetImages.squeeze(0)))
-plt.axis("off")
+fig, ax = plt.subplots(2, 2, figsize=(15, 5))
+ax[0, 0].imshow(unloader(styleImages[0].cpu().squeeze(0)))
+ax[0, 0].set_title("Style 1")
+ax[0, 0].axis("off")
+ax[0, 1].imshow(unloader(styleImages[1].cpu().squeeze(0)))
+ax[0, 1].set_title("Style 2")
+ax[0, 1].axis("off")
+ax[1, 0].imshow(unloader(contentImage.cpu().squeeze(0)))
+ax[1, 0].set_title("Content")
+ax[1, 0].axis("off")
+ax[1, 1].imshow(unloader(targetImage.cpu().squeeze(0)))
+ax[1, 1].set_title("Output")
+ax[1, 1].axis("off")
+
+plt.ioff()
 plt.show()
-
-# assert styleImage[0].size() == contentImage.size(), "Image sizes need to be the same"
-# assert styleImage[1].size() == contentImage.size(), "Image sizes need to be the same"
-
-# unloader = transforms.ToPILImage()
-
-# targetImage = runStyleTransfer(contentImage, styleImage, targetImage, 1)
-
-
-# fig, ax = plt.subplots(1, 4, figsize=(15, 5))
-# ax[0].imshow(unloader(styleImage[0].squeeze(0)))
-# ax[0].set_title("Style Image")
-# ax[0].axis("off")
-# ax[1].imshow(unloader(styleImage[1].squeeze(0)))
-# ax[1].set_title("Style Image 2")
-# ax[1].axis("off")
-# ax[2].imshow(unloader(contentImage.squeeze(0)))
-# ax[2].set_title("Content Image")
-# ax[2].axis("off")
-# ax[3].imshow(unloader(targetImage.squeeze(0)))
-# ax[3].set_title("Output Image")
-# ax[3].axis("off")
-
-# plt.ioff()
-# plt.show()
